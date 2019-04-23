529a6b3af4942b7339e448998f5311938087dce8
Sergey Klevtsov <s.klevzoff@gmail.com>
2019-04-23 18:19:13 +0300

    Fix a small bug in test.m

--- test/test.m
+++ test/test.m
@@ -11,7 +11,7 @@ n = 5;
 A = 2*eye(n)-diag(ones(n-1,1),-1)-diag(ones(n-1,1),1);
 A = kron(kron(A,eye(n)),eye(n))+kron(kron(eye(n),A),eye(n))+kron(kron(eye(n),eye(n)),A);
 
-[perm,iperm] = ReorderSparseMat(A);
+[perm,iperm] = ReorderSparseMat(A,struct());
 Aperm = A(perm,perm);
 
 NE = 5;
